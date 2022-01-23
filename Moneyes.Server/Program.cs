e495474b04cd49986b0a278e343fb772f836588f
Zigeunerich <Zigeunerich@gmail.com>
2022-01-23 13:47:44 +0100

    Fix nginx config and docker compose setup Forward headers

--- Moneyes.Server/Program.cs
+++ Moneyes.Server/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
@@ -7,11 +8,17 @@ using Microsoft.IdentityModel.Tokens;
 using Moneyes.Server;
 using Moneyes.Server.Data;
 using Moneyes.Server.Services;
+using System.Collections;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.AddEnvironmentVariables();
+
+foreach (DictionaryEntry e in Environment.GetEnvironmentVariables())
+{
+    Console.WriteLine(e.Key + ": " + e.Value);
+}
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
@@ -127,6 +134,11 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedProto
+});
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
